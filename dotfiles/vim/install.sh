e4df94fb3e641e5b31577085e7e9d0470c07c113
weipeng_yuan <5602577111ywp>
2018-10-01 03:08:51 +0800

    添加安装vim的shell脚本

---
+++ dotfiles/vim/install.sh
@@ -0,0 +1,29 @@
+# 安装vim，并添加默认配置
+# 检测vim是否已经安装
+if test ! $(rpm -qa | grep vim-enhanced)
+then
+	echo "start install vim."
+	yum -y install vim-enhanced.x86_64 >> install-log.txt
+	if test $(grep "error" install-log.txt)
+	then
+		echo "install failed."
+	else
+		echo "install success."
+		rm -rf install-log.txt
+
+		if test -e ~/.vimrc
+		then
+		    echo ".vimrc exist, rename as .vimrc-default"
+		    mv ~/.vimrc ~/.vimrc-default
+
+		    cp .vimrc ~/.vimrc
+		fi
+
+		cp .vimrc ~/.vimrc
+		echo "install and config success.
+	fi
+else
+	echo "you have installed vim"
+
+fi
+exit 0
