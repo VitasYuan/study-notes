7a8b01fe01a74f0bdb9963e89562e454f7472a6d
weipeng_yuan <5602577111ywp>
2018-10-01 10:51:22 +0800

    添加安装java和tomcat的shell脚本

---
+++ dotfiles/jdk/install.sh
@@ -0,0 +1,23 @@
+# 安装jdk8
+if test ! $(which java)
+then
+	echo "start install jdk.."
+	if test ! $(which wget)
+	then
+		yum -y install wget.x86_64 >> install-log.txt
+	fi
+
+	wget -c http://111.1.50.14/files/7194000006F691AA/download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm >> install-log.txt
+	rpm -ivh jdk-8u181-linux-x64.rpm
+
+	if test $(which java)
+	then
+		echo "install finished"
+	else
+		echo "install failed"
+		grep "error" install-log.txt
+	fi
+else
+	echo "java is already installed."
+
+fi
---
+++ dotfiles/tomcat/install.sh
@@ -0,0 +1,8 @@
+wget https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-7/v7.0.91/bin/apache-tomcat-7.0.91.tar.gz
+
+tar zxf apache-tomcat-7.0.91.tar.gz -C /root/yuanweipeng
+echo 'CATALINA_HOME=/root/yuanweipeng/apache-tomcat-7.0.91
+ PATH=$CATALINA_HOME/bin:$PATH:$HOME/bin' > /root/.bashrc
+
+# 开启防火墙端口
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
